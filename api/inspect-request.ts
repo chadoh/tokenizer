0fbea8c46b117c979e90d5151b6fb7e5c05ce2b1
Chad Ostrowski <hi@chadoh.com>
2019-12-19 10:53:52 -0500

    inspect request

--- api/inspect-request.ts
+++ api/inspect-request.ts
@@ -1,5 +1,9 @@
 import { NowRequest, NowResponse } from '@now/node'
 
 export default (req: NowRequest, res: NowResponse) => {
-  res.json({ name: 'John', email: 'john@example.com' })
+  res.json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+  })
 }
